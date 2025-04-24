5498f766a48cdd45afc6d6acf156bd1054922c4f
hamza05628 <hamidi.hamza@ofppt-edu.ma>
2025-04-24 15:56:31 +0100

    Configcloudinary storage in cloud

--- src/main/java/com/bootcamp/makemycake/config/Configcloudinary.java
+++ src/main/java/com/bootcamp/makemycake/config/Configcloudinary.java
@@ -2,7 +2,7 @@ package com.bootcamp.makemycake.config;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
-import org.springframework.beans.factory.annotation.Value;  // Correct import
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
